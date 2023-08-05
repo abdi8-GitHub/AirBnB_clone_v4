923c8a87c1ca69cb6f518ae7a2782dd654e919aa
Abdi Berhe <abdi201380@gmail.com>
2023-08-05 04:54:37 -0700

    [Update]: Filtering places by amenity

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+  const amenitiesSelected = {};
+  $('input[type="checkbox"]').change(function () {
+    const amenityId = $(this).data('id');
+    const amenityName = $(this).data('name');
+
+    if ($(this).is(':checked')) {
+      amenitiesSelected[amenityId] = amenityName;
+    } else {
+      delete amenitiesSelected[amenityId];
+    }
+    amenitiesList = Object.values(amenitiesSelected).join(', ');
+    $('div.amenities h4').text(amenitiesList);
+  });
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'ok') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
+  function fetchPlaces () {
+  const placeUrl = `http://${HOST}:5001/api/v1/places_search/`;
+    $.post({
+      url: `${HOST}/api/v1/places_search`,
+      data: JSON.stringify({}),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      success: function (response) {
+	      for (const r of response) {
+		      const article = ['<article>',
+			      '<div class="title_box">',
+			      `<h2>${r.name}</h2>`,
+			      `<div class="price_by_night">$${r.price_by_night}</div>`,
+			      '</div>',
+			      '<div class="information">',
+			      `<div class="max_guest">${r.max_guest} Guest(s)</div>`,
+			      `<div class="number_rooms">${r.number_rooms} Bedroom(s)</div>`,
+			      `<div class="number_bathrooms">${r.number_bathrooms} Bathroom(s)</div>`,
+			      '</div>',
+			      '<div class="description">',
+			      `${r.description}`,
+			      '</div>',
+			      '</article>'];
+		      $('SECTION.places').append(article.join(''));
+	      }
+      },
+      error: function (error) {
+        console.log(error);
+      }
+      });
+	  $(".filters button").bind("click", searchPlace);
+	  searchPlace();
+});
