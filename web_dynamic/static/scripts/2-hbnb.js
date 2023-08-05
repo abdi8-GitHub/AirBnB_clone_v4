16e6f5acee118344e1f352251574fc9c5883acba
Abdi Berhe <abdi201380@gmail.com>
2023-08-05 03:19:16 -0700

    [Update]: Checking the status before requesting HBNB API

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
+$(document).ready(function () {
   const amenitiesSelected = {};
-  $('input[type="checkbox"]').change(function() {
+  $('input[type="checkbox"]').change(function () {
     const amenityId = $(this).data('id');
     const amenityName = $(this).data('name');
 
@@ -12,5 +12,11 @@ $(document).ready(function() {
     amenitiesList = Object.values(amenitiesSelected).join(', ');
     $('div.amenities h4').text(amenitiesList);
   });
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'ok') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
 });
-
